e4c7fb1eaf470544012eed055a5b29776b143954
OliverMengich <kemeioliver2000@gmail.com>
2023-03-11 07:27:02 +0300

    Fixes multiple insert to Postgres

--- queries.ts
+++ queries.ts
@@ -1,6 +1,6 @@
 import {Pool} from 'pg';
 import { Request, Response } from 'express';
-import { timeStamp } from 'console';
+import config from './config';
 const pool: Pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -8,34 +8,32 @@ const pool: Pool = new Pool({
     password: 'Oliver8677',
     port: 5432,
 });
-// postgres://production_i7rh_user:27OaPfa83jPu8m3OaADlc7cPUB2ykUXs@dpg-cftosnp4reb6ks16lp30-a.oregon-postgres.render.com/production_i7rh
 const pool2 = new Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.port ,
-    ssl: true
+    user: config.user,
+    host: config.host,
+    database: config.database,
+    password: config.password,
+    port: config.port,
 })
 pool.connect(()=>console.log('Connected to DB'))
 pool2.connect(()=>console.log('Connected to Production DB'))
 export const initDB =async (req:Request,res:Response) => {
     return pool2.query(`
         CREATE TYPE Category as ENUM('Breakfast','Lunch','Snacks','Desserts','Dinner','Drinks');
-        CREATE TABLE IF NOT EXISTS "Dishes"(id uuid DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL, 
+        CREATE TABLE "Dishes"(id uuid DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL, 
         name VARCHAR(30) NOT NULL, 
         price INTEGER NOT NULL,
-        rating INTERGER NOT NULL DEFAULT 5,
-        imageUrl VARCHAR(200) NOT NULL,
+        imageUrl VARCHAR(255) NOT NULL,
         description VARCHAR(300) NOT NULL,
         category Category NOT NULL,
         featured boolean NOT NULL,
+        rating INTERGER NOT NULL DEFAULT 5,
         createdAt TIMESTAMP NOT NULL DEFAULT NOW(),
         updatedAt TIMESTAMP NOT NULL DEFAULT NOW());`,(err,response)=>{
             if (err) {
                 throw err;
             }
-            return res.status(200).json({message:" Database initiated"})
+            return res.status(200).json({message:" Database initiated"});
         })
 }
 enum Category{
@@ -48,21 +46,25 @@ enum Category{
 }
 let rs: {rating: number,id: string,createdAt:Date, category: Category,description: string, name: string, price: number, featured: boolean, updatedAt: Date,imageUrl: string}[] = [];
 const addNewDishes =async ()=>{
-    for (let i = 0; i < rs.length; i++) {
-        console.log(rs[i]);
-        const n = await pool2.query(`INSERT INTO "Dishes" values(
-            ${rs[i].id}
-            ${rs[i].name},
-            ${rs[i].price},
-            ${rs[i].imageUrl},
-            ${rs[i].description},
-            ${rs[i].category},
-            ${rs[i].featured},
-            ${rs[i].updatedAt},
-            ${rs[i].createdAt}
-            );`);
-        console.log(n);
+    try {
+        for (let i = 0; i < rs.length; i++) {
+            const n = await pool2.query(`INSERT INTO "Dishes" ("id","name","price","imageUrl","description","category","featured","rating","createdAt","updatedAt") values(
+                '${rs[i].id}',
+                '${rs[i].name}',
+                '${rs[i].price}',
+                '${rs[i].imageUrl}',
+                '${rs[i].description}',
+                '${rs[i].category}',
+                '${rs[i].featured}',
+                '${rs[i].rating}',
+                '${rs[i].createdAt.toISOString()}',
+                '${rs[i].updatedAt.toISOString()}'
+                );`);
+        }
+    } catch (error) {
+        console.log(error);
     }
+    
 }
 export const getDishes = async(req: Request,res: Response)=>{
     return pool.query('SELECT * FROM "Dishes"',(err, data)=>{
